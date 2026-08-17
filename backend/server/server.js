885a79932dcdcce9a759bc1cfde936ff8660c517
kevinn <kevinnsena353@gmail.com>
2026-08-16 23:18:53 -0500

    cambios

--- backend/server/server.js
+++ backend/server/server.js
@@ -11,8 +11,8 @@ const app = express();
 const PORT = Number(process.env.PORT || 3000);
 
 const allowedOrigins = [
-  "http://127.0.0.1:5500",
-  "http://localhost:5500"
+  "https://canchero-backend-r43k.onrender.com/api/health",
+  
 ];
 
 app.use(cors({
